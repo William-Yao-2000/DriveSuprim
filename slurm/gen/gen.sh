7a81f227b108382db4ba8ca623c4a78c4370cc2e
Zhenxin Li <1398448799@qq.com>
2025-03-23 23:27:06 +0800

    init navsim2

--- slurm/gen/gen.sh
+++ slurm/gen/gen.sh
@@ -3,7 +3,7 @@ export part=$2
 python $NAVSIM_DEVKIT_ROOT/navsim/agents/tools/gen_vocab_score.py \
 train_test_split=${split}_${part} \
 experiment_name=debug \
-worker.threads_per_node=128 \
+worker.threads_per_node=64 \
 +save_name=${split}_${part}_v2 \
 metric_cache_path=/lustre/fsw/portfolios/av/users/shiyil/zxli/navsim_workspace/exp2/${split}_metric_cache
 
