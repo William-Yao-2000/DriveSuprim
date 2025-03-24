2cabe0762e1e5f5bf20eda148d52d4a917177688
Zhenxin Li <1398448799@qq.com>
2025-03-24 19:23:26 +0800

    init navsim2

---
+++ slurm/train/train_resume.sh
@@ -0,0 +1,43 @@
+agent=$1
+dir=$1
+
+bs=32
+lr=0.0002
+epoch=20
+config="competition_training"
+
+ckpt_epoch=$2
+# Format epoch with leading zero
+padded_ckpt_epoch=$(printf "%02d" $ckpt_epoch)
+
+# Calculate step from epoch (202 steps per epoch)
+step=$((($ckpt_epoch + 1) * 202))
+resume="epoch\=${padded_ckpt_epoch}-step\=${step}.ckpt"
+
+command_string="
+    MASTER_PORT=29500 MASTER_ADDR=${MASTER_ADDR} WORLD_SIZE=${NUM_NODES} NODE_RANK=${NODE_RANK} \
+    python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
+        --config-name $config \
+        trainer.params.num_nodes=${NUM_NODES} \
+        +resume_ckpt_path='${NAVSIM_EXP_ROOT}/$dir/$resume' \
+        agent=$agent \
+        agent.pdm_gt_path=vocab_score_full_16384_navtrain_v2/navtrain.pkl \
+        experiment_name=$dir \
+        split=trainval \
+        scene_filter=navtrain \
+        dataloader.params.batch_size=$bs \
+        ~trainer.params.strategy \
+        trainer.params.max_epochs=$epoch \
+        agent.config.ckpt_path=$dir \
+        agent.lr=$lr \
+        cache_path=null
+"
+
+echo "--- COMMAND ---"
+echo $command_string
+echo "\n\n"
+
+cd $NAVSIM_DEVKIT_ROOT
+pwd
+
+eval $command_string
