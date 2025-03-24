7b573760a8637a8fbe5b461a44ae6e2e240a94e6
Zhenxin Li <1398448799@qq.com>
2025-03-24 18:14:31 +0800

    init navsim2

--- navsim/agents/tools/merge_subsets.py
+++ navsim/agents/tools/merge_subsets.py
@@ -5,14 +5,11 @@ traj_root = os.getenv('NAVSIM_TRAJPDM_ROOT')
 
 if __name__ == '__main__':
     out_dir = 'vocab_score_full_16384_navtrain_v2'
-    ins = [f'navtrain_sub{i}.pkl' for i in range(1, 65)]
+    ins = [f'navtrain_sub{i}_v2.pkl' for i in range(1, 65)]
     out = 'navtrain.pkl'
 
     result = {}
     for in_pkl in ins:
-        postfix = in_pkl.split('.')[0]
-        sub = postfix.split('_')[1]
-
         curr_pickle = pickle.load(open(f'{traj_root}/{out_dir}/{in_pkl}', 'rb'))
         print(f'{traj_root}/{out_dir}/{in_pkl}', len(curr_pickle))
         for k, v in curr_pickle.items():
