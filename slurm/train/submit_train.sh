38dbe6abddf62b2e91024e61e7194598385468d9
Zhenxin Li <1398448799@qq.com>
2025-03-25 15:13:48 +0800

    init navsim2

--- slurm/train/submit_train.sh
+++ slurm/train/submit_train.sh
@@ -12,4 +12,5 @@ submit_job \
     --email_mode never \
     --duration 4 \
     --dependent_clones 1 \
+    --partition interactive \
     -c ". slurm/pre.sh; bash slurm/train/train_auto.sh ${agent}"
