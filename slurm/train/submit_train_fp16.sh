6f43f13c63979e47c93e8b6d29a5a73aa9b5066d
Zhenxin Li <1398448799@qq.com>
2025-03-25 23:53:03 +0800

    init navsim2

---
+++ slurm/train/submit_train_fp16.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+agent=hydra_plus
+dir=hydra_plus_16mixed
+bs=32
+nodes=2
+
+#submit_job \
+#    --gpu 8 \
+#    --tasks_per_node 8 \
+#    --nodes ${nodes} \
+#    -n "zxtrain2" \
+#    --image /lustre/fsw/portfolios/av/users/shiyil/zxli/navsim_workspace/lzx-navsim.sqsh \
+#    --logroot /lustre/fsw/portfolios/av/users/shiyil/zxli/navsim_workspace/slurm_logs \
+#    --email_mode never \
+#    --duration 4 \
+#    --dependent_clones 2 \
+#    -c ". slurm/pre.sh; bash slurm/train/train_auto.sh ${agent} ${dir} ${bs}"
+
+submit_job \
+    --gpu 8 \
+    --tasks_per_node 8 \
+    --nodes ${nodes} \
+    -n "zxtrain2" \
+    --image /lustre/fsw/portfolios/av/users/shiyil/zxli/navsim_workspace/lzx-navsim.sqsh \
+    --logroot /lustre/fsw/portfolios/av/users/shiyil/zxli/navsim_workspace/slurm_logs \
+    --email_mode never \
+    --duration 4 \
+    --dependent_clones 2 \
+    -c ". slurm/pre.sh; bash slurm/train/train_auto.sh ${agent} ${dir} ${bs}"
