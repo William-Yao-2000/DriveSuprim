cc22dc412a37579fb04f9b27916251ede9037332
Zhenxin Li <1398448799@qq.com>
2025-04-05 12:19:07 +0800

    ep debug

--- slurm/cache/dp.sh
+++ slurm/cache/dp.sh
@@ -1,6 +1,6 @@
 export agent=dp
 export res="2048x512"
-python navsim/planning/script/run_dataset_caching.py \
+python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_dataset_caching.py \
 --config-name competition_training \
 agent=${agent} \
 agent.pdm_gt_path=1234 \
