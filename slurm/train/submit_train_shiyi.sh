723741523f1f18b6e452367843fd2975918764d2
Zhenxin Li <1398448799@qq.com>
2025-04-06 02:32:29 +0800

    ep debug

---
+++ slurm/train/submit_train_shiyi.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#agent=hydra_plus
+#dir=hydra_plus_video
+#bs=22
+#nodes=3
+#epochs=20
+
+agent=dp
+dir=dp_ctrl
+bs=32
+nodes=2
+epochs=100
+
+submit_job \
+    --gpu 8 \
+    --tasks_per_node 8 \
+    --account av_alpamayo_research \
+    --nodes ${nodes} \
+    -n bash \
+    --image /lustre/fsw/portfolios/av/users/zhenxinl/navsim_workspace/lzx-navsim.sqsh \
+    --logroot /lustre/fsw/portfolios/av/users/zhenxinl/navsim_workspace/slurm_logs \
+    --email_mode never \
+    --duration 4 \
+    --dependent_clones 2 \
+    --partition interactive \
+    -c ". slurm/pre_shiyi.sh; bash slurm/train/train_auto_fp32.sh ${agent} ${dir} ${bs} ${epochs}"
