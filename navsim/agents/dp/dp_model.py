cddaf412c6006bb901288c43d43e131c232f6d07
Zhenxin Li <1398448799@qq.com>
2025-04-06 18:23:30 +0800

    dp inference hydra one frame

--- navsim/agents/dp/dp_model.py
+++ navsim/agents/dp/dp_model.py
@@ -266,7 +266,7 @@ class DPHead(nn.Module):
             beta_end=0.02,
             beta_schedule='squaredcos_cap_v2',
             variance_type='fixed_small',
-            clip_sample=True,
+            clip_sample=False,
             prediction_type='epsilon'
         )
         img_num = 2 if config.use_back_view else 1
