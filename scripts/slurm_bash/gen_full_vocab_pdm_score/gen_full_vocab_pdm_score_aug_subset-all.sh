46a69f9e0c3d2b0725d2c92011819f9cfdf205b7
william_yao_2000 <281744213@qq.com>
2025-04-09 04:31:17 +0000

    fix bugs

--- scripts/slurm_bash/gen_full_vocab_pdm_score/gen_full_vocab_pdm_score_aug_subset-all.sh
+++ scripts/slurm_bash/gen_full_vocab_pdm_score/gen_full_vocab_pdm_score_aug_subset-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 start_=$1
 end_=$2
-seed=$3
-partition=$4
+seed_=$3
+partition_=$4
 
 for idx in $(seq $start_ $end_)
 do
@@ -10,15 +10,15 @@ do
         --gpu 1 \
         --tasks_per_node 1 \
         --nodes 1 \
-        -n "gen_full_vocab_pdm_score_navtrain_sub${idx}_seed_$seed" \
+        -n "gen_full_vocab_pdm_score_navtrain_sub${idx}_seed_$seed_" \
         --image /lustre/fsw/portfolios/av/users/shiyil/yaowenh/container_images/ywh-navsim.sqsh \
         --logroot /lustre/fsw/portfolios/av/users/shiyil/yaowenh/slurm_logs/gen_full_vocab_pdm_score \
         --email_mode never \
         --duration 4 \
         --dependent_clones 0 \
         --account av_research \
-        --partition $partition
+        --partition $partition_
         -c ". /lustre/fsw/portfolios/av/users/shiyil/yaowenh/pre-navsim_v2.sh; \
-        bash scripts/ssl/gen_full_score_aug/gen_training_full_score_aug_subset-seeds.sh navtrain_sub$idx $seed"
+        bash scripts/ssl/gen_full_score_aug/gen_training_full_score_aug_subset-seeds.sh navtrain_sub$idx $seed_"
     sleep 3
 done
