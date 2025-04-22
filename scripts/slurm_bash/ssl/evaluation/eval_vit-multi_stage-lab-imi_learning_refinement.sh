0b6755221a0ad0faa834a0fa1cc5b827107a71ba
william_yao_2000 <281744213@qq.com>
2025-04-22 08:01:28 +0000

    new bash

---
+++ scripts/slurm_bash/ssl/evaluation/eval_vit-multi_stage-lab-imi_learning_refinement.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+epoch=$1
+dir=$2
+partition=$3
+
+
+dir_name=$(echo $dir | tr '/' '-' | tr '.' 'dot')
+
+submit_job \
+    --gpu 8 \
+    --tasks_per_node 8 \
+    --nodes 1 \
+    -n "eval-navsim_ssl-$dir_name-${epoch}epoch" \
+    --image /lustre/fsw/portfolios/av/users/shiyil/yaowenh/container_images/ywh-navsim.sqsh \
+    --logroot /lustre/fsw/portfolios/av/users/shiyil/yaowenh/slurm_logs/navsim_v2/evaluation/ \
+    --email_mode never \
+    --duration 4 \
+    --dependent_clones 0 \
+    --partition $partition \
+    --account av_research \
+    -c ". /lustre/fsw/portfolios/av/users/shiyil/yaowenh/pre-navsim_v2.sh; bash scripts/ssl/evaluation/eval_vit-multi_stage-lab-imi_learning_refinement.sh $epoch $dir"
