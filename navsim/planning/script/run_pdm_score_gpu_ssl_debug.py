741546e2f4a0a42f50a1b58cb3a795110a274759
william_yao_2000 <281744213@qq.com>
2025-04-27 07:26:47 +0000

    small revisions

---
+++ navsim/planning/script/run_pdm_score_gpu_ssl_debug.py
@@ -0,0 +1,389 @@
+"""
+Two stage eval for warmup
+"""
+
+import logging
+import os
+import pickle
+import traceback
+import uuid
+from dataclasses import fields
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List, Tuple, Union
+
+import hydra
+import pandas as pd
+import pytorch_lightning as pl
+import torch.distributed as dist
+from hydra.utils import instantiate
+from nuplan.common.actor_state.state_representation import StateSE2
+from nuplan.common.geometry.convert import relative_to_absolute_poses
+from nuplan.planning.script.builders.logging_builder import build_logger
+from nuplan.planning.utils.multithreading.worker_utils import worker_map
+from omegaconf import DictConfig
+from torch.utils.data import DataLoader
+
+from navsim.agents.abstract_agent import AbstractAgent
+from navsim.common.dataclasses import PDMResults, SensorConfig
+from navsim.common.dataloader import MetricCacheLoader, SceneFilter, SceneLoader
+from navsim.common.enums import SceneFrameType
+from navsim.evaluate.pdm_score import pdm_score
+from navsim.planning.script.builders.worker_pool_builder import build_worker
+from navsim.planning.script.run_pdm_score import compute_final_scores, calculate_weighted_average_score, \
+                                                 calculate_individual_mapping_scores, create_scene_aggregators
+from navsim.planning.simulation.planner.pdm_planner.scoring.pdm_scorer import PDMScorer
+from navsim.planning.simulation.planner.pdm_planner.simulation.pdm_simulator import PDMSimulator
+from navsim.planning.training.agent_lightning_module_ssl import AgentLightningModuleSSL
+from navsim.planning.training.dataset_ssl import DatasetSSL as Dataset
+from navsim.traffic_agents_policies.abstract_traffic_agents_policy import AbstractTrafficAgentsPolicy
+
+logger = logging.getLogger(__name__)
+
+CONFIG_PATH = "config/pdm_scoring"
+CONFIG_NAME = "default_run_pdm_score_gpu_ssl"
+
+
+# TODO gpu inference
+
+def run_pdm_score_wo_inference(args: List[Dict[str, Union[List[str], DictConfig]]]) -> List[pd.DataFrame]:
+    """
+    Helper function to run PDMS evaluation in.
+    :param args: input arguments
+    """
+    if os.getenv('ROBUST_HYDRA_DEBUG') == 'true':
+        import pdb; pdb.set_trace()
+    node_id = int(os.environ.get("NODE_RANK", 0))
+    thread_id = str(uuid.uuid4())
+    logger.info(f"Starting worker in thread_id={thread_id}, node_id={node_id}")
+
+    log_names = [a["log_file"] for a in args]
+    tokens = [t for a in args for t in a["tokens"]]
+    cfg: DictConfig = args[0]["cfg"]
+    # model_trajectory = args[0]['model_trajectory']
+
+    simulator: PDMSimulator = instantiate(cfg.simulator)
+    scorer: PDMScorer = instantiate(cfg.scorer)
+    assert (
+            simulator.proposal_sampling == scorer.proposal_sampling
+    ), "Simulator and scorer proposal sampling has to be identical"
+    # traffic_agents_policy: AbstractTrafficAgentsPolicy = instantiate(
+    #     cfg.traffic_agents_policy, simulator.proposal_sampling
+    # )
+    
+    metric_cache_loader = MetricCacheLoader(Path(cfg.metric_cache_path))
+    scene_filter: SceneFilter = instantiate(cfg.train_test_split.scene_filter)
+    scene_filter.log_names = log_names
+    scene_filter.tokens = tokens
+    scene_loader = SceneLoader(
+        synthetic_sensor_path=Path(cfg.synthetic_sensor_path),
+        original_sensor_path=Path(cfg.original_sensor_path),
+        data_path=Path(cfg.navsim_log_path),
+        synthetic_scenes_path=Path(cfg.synthetic_scenes_path),
+        scene_filter=scene_filter,
+    )
+
+    pdm_results: List[pd.DataFrame] = []
+
+    # first stage
+
+    traffic_agents_policy_stage_one: AbstractTrafficAgentsPolicy = instantiate(
+        cfg.traffic_agents_policy.reactive, simulator.proposal_sampling
+    )
+
+    scene_loader_tokens_stage_one = scene_loader.tokens_stage_one
+
+    tokens_to_evaluate_stage_one = list(set(scene_loader_tokens_stage_one) & set(metric_cache_loader.tokens))
+
+    for idx, (token) in enumerate(tokens_to_evaluate_stage_one):
+        logger.info(
+            f"Processing scenario {idx + 1} / {len(tokens_to_evaluate_stage_one)} in thread_id={thread_id}, node_id={node_id}"
+        )
+        try:
+            metric_cache = metric_cache_loader.get_from_token(token)
+            score_row_stage_one = {}
+            # trajectory = model_trajectory[token]['trajectory']
+            # score_row_stage_one, ego_simulated_states = pdm_score(
+            #     metric_cache=metric_cache,
+            #     model_trajectory=trajectory,
+            #     future_sampling=simulator.proposal_sampling,
+            #     simulator=simulator,
+            #     scorer=scorer,
+            #     traffic_agents_policy=traffic_agents_policy_stage_one,
+            # )
+            # score_row_stage_one["valid"] = True
+            # score_row_stage_one["log_name"] = metric_cache.log_name
+            # score_row_stage_one["frame_type"] = metric_cache.scene_type
+            # score_row_stage_one["start_time"] = metric_cache.timepoint.time_s
+            # end_pose = StateSE2(
+            #     x=trajectory.poses[-1, 0],
+            #     y=trajectory.poses[-1, 1],
+            #     heading=trajectory.poses[-1, 2],
+            # )
+            # absolute_endpoint = relative_to_absolute_poses(metric_cache.ego_state.rear_axle, [end_pose])[0]
+            # score_row_stage_one["endpoint_x"] = absolute_endpoint.x
+            # score_row_stage_one["endpoint_y"] = absolute_endpoint.y
+            # score_row_stage_one["start_point_x"] = metric_cache.ego_state.rear_axle.x
+            # score_row_stage_one["start_point_y"] = metric_cache.ego_state.rear_axle.y
+            # score_row_stage_one["ego_simulated_states"] = [ego_simulated_states]  # used for two-frames extended comfort
+
+        except Exception:
+            logger.warning(f"----------- Agent failed for token {token}:")
+            traceback.print_exc()
+            score_row_stage_one = pd.DataFrame([PDMResults.get_empty_results()])
+            score_row_stage_one["valid"] = False
+        score_row_stage_one["token"] = token
+
+        pdm_results.append(score_row_stage_one)
+
+    # second stage
+
+    traffic_agents_policy_stage_two: AbstractTrafficAgentsPolicy = instantiate(
+        cfg.traffic_agents_policy.reactive, simulator.proposal_sampling
+    )
+    scene_loader_tokens_stage_two = scene_loader.reactive_tokens_stage_two
+
+    tokens_to_evaluate_stage_two = list(set(scene_loader_tokens_stage_two) & set(metric_cache_loader.tokens))
+    for idx, (token) in enumerate(tokens_to_evaluate_stage_two):
+        logger.info(
+            f"Processing stage two reactive scenario {idx + 1} / {len(tokens_to_evaluate_stage_two)} in thread_id={thread_id}, node_id={node_id}"
+        )
+        try:
+            metric_cache = metric_cache_loader.get_from_token(token)
+            # trajectory = model_trajectory[token]['trajectory']
+            score_row_stage_two = {}
+            # score_row_stage_two, ego_simulated_states = pdm_score(
+            #     metric_cache=metric_cache,
+            #     model_trajectory=trajectory,
+            #     future_sampling=simulator.proposal_sampling,
+            #     simulator=simulator,
+            #     scorer=scorer,
+            #     traffic_agents_policy=traffic_agents_policy_stage_two,
+            # )
+            # score_row_stage_two["valid"] = True
+            # score_row_stage_two["log_name"] = metric_cache.log_name
+            # score_row_stage_two["frame_type"] = metric_cache.scene_type
+            # score_row_stage_two["start_time"] = metric_cache.timepoint.time_s
+            # end_pose = StateSE2(
+            #     x=trajectory.poses[-1, 0],
+            #     y=trajectory.poses[-1, 1],
+            #     heading=trajectory.poses[-1, 2],
+            # )
+            # absolute_endpoint = relative_to_absolute_poses(metric_cache.ego_state.rear_axle, [end_pose])[0]
+            # score_row_stage_two["endpoint_x"] = absolute_endpoint.x
+            # score_row_stage_two["endpoint_y"] = absolute_endpoint.y
+            # score_row_stage_two["start_point_x"] = metric_cache.ego_state.rear_axle.x
+            # score_row_stage_two["start_point_y"] = metric_cache.ego_state.rear_axle.y
+            # score_row_stage_two["ego_simulated_states"] = [ego_simulated_states]  # used for two-frames extended comfort
+
+        except Exception:
+            logger.warning(f"----------- Agent failed for token {token}:")
+            traceback.print_exc()
+            score_row_stage_two = pd.DataFrame([PDMResults.get_empty_results()])
+            score_row_stage_two["valid"] = False
+        score_row_stage_two["token"] = token
+
+        pdm_results.append(score_row_stage_two)
+
+    return pdm_results
+
+
+@hydra.main(config_path=CONFIG_PATH, config_name=CONFIG_NAME, version_base=None)
+def main(cfg: DictConfig) -> None:
+    """
+    Main entrypoint for running PDMS evaluation.
+    :param cfg: omegaconf dictionary
+    """
+
+    build_logger(cfg)
+
+    if cfg.debug:
+        import pdb; pdb.set_trace()
+        import os; os.environ['ROBUST_HYDRA_DEBUG'] = 'true'
+
+    # gpu inference
+    # agent: AbstractAgent = instantiate(cfg.agent)
+    # agent.initialize()
+    # # Extract scenes based on scene-loader to know which tokens to distribute across workers
+    scene_filter = instantiate(cfg.train_test_split.scene_filter)
+    # scene_loader_inference = SceneLoader(
+    #     synthetic_sensor_path=Path(cfg.synthetic_sensor_path),
+    #     original_sensor_path=Path(cfg.original_sensor_path),
+    #     data_path=Path(cfg.navsim_log_path),
+    #     synthetic_scenes_path=Path(cfg.synthetic_scenes_path),
+    #     scene_filter=scene_filter,
+    #     sensor_config=agent.get_sensor_config(),
+    # )
+    # dataset = Dataset(
+    #     scene_loader=scene_loader_inference,
+    #     feature_builders=agent.get_feature_builders(),
+    #     target_builders=agent.get_target_builders(),
+    #     cfg=cfg.agent.config,
+    #     cache_path=None,
+    #     force_cache_computation=False,
+    #     append_token_to_batch=True
+    # )
+    # dataloader = DataLoader(dataset, **cfg.dataloader.params, shuffle=False)
+
+    # Extract scenes based on scene-loader to know which tokens to distribute across workers
+    # TODO: infer the tokens per log from metadata, to not have to load metric cache and scenes here
+    scene_loader = SceneLoader(
+        synthetic_sensor_path=None,
+        original_sensor_path=None,
+        data_path=Path(cfg.navsim_log_path),
+        synthetic_scenes_path=Path(cfg.synthetic_scenes_path),
+        scene_filter=scene_filter,
+        sensor_config=SensorConfig.build_no_sensors(),
+    )
+    metric_cache_loader = MetricCacheLoader(Path(cfg.metric_cache_path))
+
+    tokens_to_evaluate = list(set(scene_loader.tokens) & set(metric_cache_loader.tokens))
+    num_missing_metric_cache_tokens = len(set(scene_loader.tokens) - set(metric_cache_loader.tokens))
+    num_unused_metric_cache_tokens = len(set(metric_cache_loader.tokens) - set(scene_loader.tokens))
+    if num_missing_metric_cache_tokens > 0:
+        logger.warning(f"Missing metric cache for {num_missing_metric_cache_tokens} tokens. Skipping these tokens.")
+    if num_unused_metric_cache_tokens > 0:
+        logger.warning(f"Unused metric cache for {num_unused_metric_cache_tokens} tokens. Skipping these tokens.")
+    logger.info(f"Starting pdm scoring of {len(tokens_to_evaluate)} scenarios...")
+
+    # assert len(dataset) == len(tokens_to_evaluate), f'dataloader: {len(dataset)}, tokens: {len(tokens_to_evaluate)}'
+
+    # trainer = pl.Trainer(**cfg.trainer.params, callbacks=agent.get_training_callbacks())
+    # predictions = trainer.predict(
+    #     AgentLightningModuleSSL(
+    #         cfg=cfg.agent.config,
+    #         agent=agent,
+    #     ),
+    #     dataloader,
+    #     return_predictions=True
+    # )
+
+    # dist.barrier()
+    # all_predictions = [None for _ in range(dist.get_world_size())]
+
+    # if dist.is_initialized():
+    #     dist.all_gather_object(all_predictions, predictions)
+    # else:
+    #     all_predictions.append(predictions)
+
+    # if dist.get_rank() != 0:
+    #     return None
+
+    # merged_predictions = {}
+    # for proc_prediction in all_predictions:
+    #     for d in proc_prediction:
+    #         merged_predictions.update(d)
+
+    # agent_ckpt_path = Path(cfg.agent.checkpoint_path).parent.absolute().__str__()
+    # ckpt_name = Path(cfg.agent.checkpoint_path).name.split('.')[0]
+    # pickle.dump(merged_predictions, open(f'{agent_ckpt_path}/{ckpt_name}.pkl', 'wb'))
+
+    data_points = [
+        {
+            "cfg": cfg,
+            "log_file": log_file,
+            "tokens": tokens_list,
+            # "model_trajectory": merged_predictions
+        }
+        for log_file, tokens_list in scene_loader.get_tokens_list_per_log().items()
+    ]
+
+    if os.getenv('ROBUST_HYDRA_DEBUG') == 'true':
+        import pdb; pdb.set_trace()
+    worker = build_worker(cfg)
+    score_rows: List[pd.DataFrame] = worker_map(worker, run_pdm_score_wo_inference, data_points)
+
+    pdm_score_df = pd.concat(score_rows)
+    
+    try:
+        raw_mapping = cfg.train_test_split.reactive_all_mapping
+        all_mappings: Dict[Tuple[str, str], List[Tuple[str, str]]] = {}
+
+        for orig_token, prev_token, two_stage_pairs in raw_mapping:
+            if prev_token in set(scene_loader.tokens) or orig_token in set(scene_loader.tokens):
+                all_mappings[(orig_token, prev_token)] = [tuple(pair) for pair in two_stage_pairs]
+
+        pdm_score_df = create_scene_aggregators(
+            all_mappings, pdm_score_df, instantiate(cfg.simulator.proposal_sampling)
+        )
+        pdm_score_df = compute_final_scores(pdm_score_df)
+        pseudo_closed_loop_valid = True
+
+    except Exception:
+        logger.warning("----------- Failed to calculate pseudo closed-loop weights:")
+        traceback.print_exc()
+        pdm_score_df["weight"] = 1.0
+        pseudo_closed_loop_valid = False
+
+    num_sucessful_scenarios = pdm_score_df["valid"].sum()
+    num_failed_scenarios = len(pdm_score_df) - num_sucessful_scenarios
+    if num_failed_scenarios > 0:
+        failed_tokens = pdm_score_df[~pdm_score_df["valid"]]["token"].to_list()
+    else:
+        failed_tokens = []
+
+    score_cols = [
+        c
+        for c in pdm_score_df.columns
+        if (
+            (any(score.name in c for score in fields(PDMResults)) or c == "two_frame_extended_comfort" or c == "score")
+            and c != "pdm_score"
+        )
+    ]
+
+    # Calculate average score of all frames
+    average_all_frames_extended_pdm_score_row = pdm_score_df[score_cols].mean(skipna=True)
+    average_all_frames_extended_pdm_score_row["token"] = "average_all_frames_extended_pdm_score"
+    average_all_frames_extended_pdm_score_row["valid"] = pdm_score_df["valid"].all()
+
+    # Calculate pseudo closed loop score with weighted average
+    extended_pdm_score_row = calculate_individual_mapping_scores(
+        pdm_score_df[score_cols + ["token", "weight"]], all_mappings
+    )
+    extended_pdm_score_row["token"] = "extended_pdm_score"
+    extended_pdm_score_row["valid"] = pseudo_closed_loop_valid
+
+    # Original frames average
+    original_frames = pdm_score_df[pdm_score_df["frame_type"] == SceneFrameType.ORIGINAL]
+    average_expert_frames_extended_pdm_score_row = original_frames[score_cols].mean(skipna=True)
+    average_expert_frames_extended_pdm_score_row["token"] = "average_expert_frames_extended_pdm_score"
+    average_expert_frames_extended_pdm_score_row["valid"] = original_frames["valid"].all()
+
+    # append average and pseudo closed loop scores
+    pdm_score_df = pdm_score_df[["token", "valid"] + score_cols]
+    pdm_score_df.loc[len(pdm_score_df)] = average_all_frames_extended_pdm_score_row
+    pdm_score_df.loc[len(pdm_score_df)] = average_expert_frames_extended_pdm_score_row
+    pdm_score_df.loc[len(pdm_score_df)] = extended_pdm_score_row
+
+    save_path = Path(cfg.output_dir)
+    timestamp = datetime.now().strftime("%Y.%m.%d.%H.%M.%S")
+    pdm_score_df.to_csv(save_path / f"{timestamp}.csv")
+
+    logger.info(
+        f"""
+        Finished running evaluation.
+            Number of successful scenarios: {num_sucessful_scenarios}.
+            Number of failed scenarios: {num_failed_scenarios}.
+            Final extended pdm score of valid results: {pdm_score_df[pdm_score_df["token"] == "extended_pdm_score"]["score"].iloc[0]}.
+            Results are stored in: {save_path / f"{timestamp}.csv"}.
+        """
+    )
+
+    if cfg.verbose:
+        logger.info(
+            f"""
+            Detailed results:
+            {pdm_score_df.iloc[-3:].T}
+            """
+        )
+    if num_failed_scenarios > 0:
+        logger.info(
+            f"""
+            List of failed tokens:
+            {failed_tokens}
+            """
+        )
+
+
+if __name__ == "__main__":
+    main()
