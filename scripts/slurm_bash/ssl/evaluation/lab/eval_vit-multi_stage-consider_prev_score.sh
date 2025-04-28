11cb4595602f1a0935e3b676e7c334532ec79725
william_yao_2000 <281744213@qq.com>
2025-04-28 13:12:39 +0000

    new bash

---
+++ scripts/slurm_bash/ssl/evaluation/lab/eval_vit-multi_stage-consider_prev_score.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+epoch=$1
+dir=$2
+partition=$3
+
+dir_name=$(echo $dir | tr '/' '-' | tr '.' 'dot')
+
+submit_job \
+    --gpu 8 \
+    --tasks_per_node 8 \
+    --nodes 1 \
+    -n "eval-navsim_ssl-$dir_name-${epoch}epoch-consider_prev_score" \
+    --image /lustre/fsw/portfolios/av/users/shiyil/yaowenh/container_images/ywh-navsim.sqsh \
+    --logroot /lustre/fsw/portfolios/av/users/shiyil/yaowenh/slurm_logs/navsim_v2/evaluation/ \
+    --email_mode never \
+    --duration 4 \
+    --dependent_clones 0 \
+    --partition $partition \
+    --account av_research \
+    -c ". /lustre/fsw/portfolios/av/users/shiyil/yaowenh/pre-navsim_v2.sh; bash scripts/ssl/evaluation/lab/eval_vit-multi_stage-consider_prev_score.sh $epoch $dir"
+
+
+: '
+usage:
+bash scripts/slurm_bash/ssl/evaluation/lab/eval_vit-multi_stage-consider_prev_score.sh \
+    2 rot_30-trans_0-va_0-p_0.5/multi_stage/labs/stage_layers_3-topks_256-optimize_prev_frame_traj_for_ec_v2 \
+    interactive_singlenode
+'
---
+++ scripts/ssl/evaluation/lab/eval_vit-multi_stage-consider_prev_score.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Default values
+epoch=${1:-19}
+dir=${2:-"training/ssl/ori/lr_baseline"}
+num_refinement_stage=1
+stage_layers=3
+topks=256
+
+inference_model="teacher"
+
+# Format epoch with leading zero
+padded_epoch=$(printf "%02d" $epoch)
+
+# Calculate step from epoch (1330 steps per epoch)
+step=$((($epoch + 1) * 1330))
+
+metric_cache_path="${NAVSIM_EXP_ROOT}/metric_cache/test/ori"
+
+# Set experiment name based on inference model
+if [ "$inference_model" = "teacher" ]; then
+    experiment_name="${dir}/test-${padded_epoch}ep-one_stage-consider_prev_score"
+else
+    experiment_name="${dir}/test-${padded_epoch}ep-${inference_model}-one_stage-consider_prev_score"
+fi
+
+command_string="TORCH_NCCL_ENABLE_MONITORING=0 \
+python ${NAVSIM_DEVKIT_ROOT}/navsim/planning/script/run_pdm_score_one_stage_gpu_ssl.py \
+    +debug=false \
+    +use_pdm_closed=false \
+    agent=hydra_img_vit_ssl \
+    dataloader.params.batch_size=8 \
+    worker.threads_per_node=128 \
+    agent.checkpoint_path='${NAVSIM_EXP_ROOT}/${dir}/epoch\=${padded_epoch}-step\=${step}.ckpt' \
+    agent.config.training=false \
+    agent.config.only_ori_input=true \
+    agent.config.inference.model=${inference_model} \
+    agent.config.refinement.use_multi_stage=true \
+    agent.config.refinement.num_refinement_stage=$num_refinement_stage \
+    agent.config.refinement.stage_layers=$stage_layers \
+    agent.config.refinement.topks=$topks \
+    agent.config.lab.use_first_stage_traj_in_infer=false \
+    agent.config.lab.optimize_prev_frame_traj_for_ec=true \
+    experiment_name=${experiment_name} \
+    +cache_path=null \
+    metric_cache_path=${metric_cache_path} \
+    train_test_split=navtest \
+"
+
+echo "--- COMMAND ---"
+echo $command_string
+echo -e "\n\n"
+
+eval $command_string
+
+
+: '
+usage:
+bash scripts/ssl/evaluation/eval_vit-multi_stage.sh \
+    3 training/ssl/teacher_student/rot_30-trans_0-va_0-p_0.5/multi_stage/stage_layers_3+3-topks_256+64 \
+    2 3+3 256+64
+'
