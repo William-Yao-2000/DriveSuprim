6ac4c40ecfd1490e4b231b0f4148d288a7ed5519
william_yao_2000 <281744213@qq.com>
2025-04-30 07:19:31 +0000

    small revisions

--- download/download_navhard_two_stage.sh
+++ download/download_navhard_two_stage.sh
@@ -1,9 +1,12 @@
 wget -c https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
 wget -c https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
 wget -c https://huggingface.co/datasets/OpenDriveLab/OpenScene/resolve/main/navsim-v2/navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
-tar -xzvf navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
-tar -xzvf navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
-tar -xzvf navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+
+tar -xzf navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
+tar -xzf navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
+tar -xzf navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
+
+
 rm navsim_v2.2_navhard_two_stage_curr_sensors.tar.gz
 rm navsim_v2.2_navhard_two_stage_hist_sensors.tar.gz
 rm navsim_v2.2_navhard_two_stage_scene_pickles.tar.gz
