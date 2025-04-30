d36057881091f5f16d0eb585a455ea997ee43e45
william_yao_2000 <281744213@qq.com>
2025-04-30 08:11:25 +0000

    small revisions

--- download/download_warmup_synthetic_scenes.sh
+++ download/download_warmup_synthetic_scenes.sh
@@ -1,3 +1,3 @@
-wget https://huggingface.co/datasets/AGC2025/warmup_synthetic_scenes/resolve/main/synthetic_scenes.tar.gz
+wget -c https://huggingface.co/datasets/AGC2025/warmup_synthetic_scenes/resolve/main/synthetic_scenes.tar.gz
 tar -xzvf synthetic_scenes.tar.gz
 rm synthetic_scenes.tar.gz
