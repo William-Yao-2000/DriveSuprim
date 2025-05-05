99e433d7e03441b2bd5d40f3e4a529cd62e10ef0
william_yao_2000 <281744213@qq.com>
2025-05-05 04:42:50 +0000

    fix bugs

--- scripts/evaluation/run_human_agent_pdm_score_evaluation.sh
+++ scripts/evaluation/run_human_agent_pdm_score_evaluation.sh
@@ -5,5 +5,4 @@ python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score_one_stage.py \
     train_test_split=$TRAIN_TEST_SPLIT \
     agent=human_agent \
     experiment_name=training/human_agent/test-one_stage \
-    traffic_agents_policy=non_reactive \
     metric_cache_path=$CACHE_PATH \
