4121963b5c975d9f87afe204f50525aac61e9dec
william_yao_2000 <281744213@qq.com>
2025-05-05 04:30:09 +0000

    human traj eval

--- scripts/evaluation/run_human_agent_pdm_score_evaluation.sh
+++ scripts/evaluation/run_human_agent_pdm_score_evaluation.sh
@@ -1,9 +1,9 @@
 TRAIN_TEST_SPLIT=navtest
-CACHE_PATH=YOUR_PATH_TO_METRIC_CACHE
+CACHE_PATH="${NAVSIM_EXP_ROOT}/metric_cache/test/ori"
 
-python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score.py \
-train_test_split=$TRAIN_TEST_SPLIT \
-agent=human_agent \
-experiment_name=human_agent \
-traffic_agents_policy=non_reactive \
-metric_cache_path=$CACHE_PATH \
+python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score_one_stage.py \
+    train_test_split=$TRAIN_TEST_SPLIT \
+    agent=human_agent \
+    experiment_name=training/human_agent/test-one_stage \
+    traffic_agents_policy=non_reactive \
+    metric_cache_path=$CACHE_PATH \
