6814b8359a9bf560ec9f742232d84ede3d80d47b
william_yao_2000 <281744213@qq.com>
2025-05-05 02:07:35 +0000

    add agent

--- scripts/ssl/evaluation/eval_vit-multi_stage.sh
+++ scripts/ssl/evaluation/eval_vit-multi_stage.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Default values
-epoch=${1:-19}
+epoch=$1
 dir=${2:-"training/ssl/ori/lr_baseline"}
 num_refinement_stage=$3
 stage_layers=$4
 topks=$5
+agent=${6:-"hydra_img_vit_ssl"}
+use_first_stage_traj_in_infer=${7:-"false"}
 
 inference_model="teacher"
 
@@ -24,11 +26,15 @@ else
     experiment_name="${dir}/test-${padded_epoch}ep-${inference_model}-one_stage"
 fi
 
+if [ "$use_first_stage_traj_in_infer" = "true" ]; then
+    experiment_name="$experiment_name-use_first_stage_traj_in_infer"
+fi
+
 command_string="TORCH_NCCL_ENABLE_MONITORING=0 \
 python ${NAVSIM_DEVKIT_ROOT}/navsim/planning/script/run_pdm_score_one_stage_gpu_ssl.py \
     +debug=false \
     +use_pdm_closed=false \
-    agent=hydra_img_vit_ssl \
+    agent=$agent \
     dataloader.params.batch_size=8 \
     worker.threads_per_node=128 \
     agent.checkpoint_path='${NAVSIM_EXP_ROOT}/${dir}/epoch\=${padded_epoch}-step\=${step}.ckpt' \
