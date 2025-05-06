b7feb1438fee0d25e052475c01b0b2b7bb8cdc79
william_yao_2000 <281744213@qq.com>
2025-05-06 02:41:12 +0000

    fix bugs

--- scripts/training/run_ego_mlp_agent_training.sh
+++ scripts/training/run_ego_mlp_agent_training.sh
@@ -2,5 +2,7 @@ TRAIN_TEST_SPLIT=navtrain
 
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
     experiment_name=training/ego_mlp_agent \
+    ~trainer.params.strategy \
     trainer.params.max_epochs=50 \
+    trainer.params.limit_val_batches=0.05 \
     train_test_split=$TRAIN_TEST_SPLIT \
--- scripts/training/run_transfuser_training.sh
+++ scripts/training/run_transfuser_training.sh
@@ -3,4 +3,6 @@ TRAIN_TEST_SPLIT=navtrain
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
     agent=transfuser_agent \
     experiment_name=training/transfuser_agent \
+    ~trainer.params.strategy \
+    trainer.params.limit_val_batches=0.05 \
     train_test_split=$TRAIN_TEST_SPLIT \
