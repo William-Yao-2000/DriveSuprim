001275590042ac7a621ebdae329309592428d5bd
william_yao_2000 <281744213@qq.com>
2025-05-06 13:15:54 +0000

    small revisions

--- scripts/training/run_ego_mlp_agent_training.sh
+++ scripts/training/run_ego_mlp_agent_training.sh
@@ -5,7 +5,7 @@ python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
     experiment_name=training/ego_mlp_agent \
     ~trainer.params.strategy \
     trainer.params.max_epochs=50 \
-    trainer.params.limit_val_batches=0.05 \
+    trainer.params.limit_val_batches=0.2 \
     train_test_split=$TRAIN_TEST_SPLIT \
     cache_path=null
 
