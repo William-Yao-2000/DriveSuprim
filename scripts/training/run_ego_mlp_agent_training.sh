0b791d21fcc51dd9f8c084575c14390d87025d2f
william_yao_2000 <281744213@qq.com>
2025-05-06 03:58:04 +0000

    fix bugs

--- scripts/training/run_ego_mlp_agent_training.sh
+++ scripts/training/run_ego_mlp_agent_training.sh
@@ -1,9 +1,24 @@
 TRAIN_TEST_SPLIT=navtrain
 
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
+    +debug=false \
     experiment_name=training/ego_mlp_agent \
     ~trainer.params.strategy \
     trainer.params.max_epochs=50 \
     trainer.params.limit_val_batches=0.05 \
     train_test_split=$TRAIN_TEST_SPLIT \
     cache_path=null
+
+
+: '
+debug:
+
+CUDA_VISIBLE_DEVICES=0 HYDRA_FULL_ERROR=1 \
+python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
+    +debug=true \
+    experiment_name=debug \
+    \~trainer.params.strategy \
+    trainer.params.max_epochs=50 \
+    train_test_split=navtrain_debug \
+    cache_path=null
+'
