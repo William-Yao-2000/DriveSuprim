592d5d7e782bae72bd0de5c37f206b06e32b5e10
william_yao_2000 <281744213@qq.com>
2025-05-08 04:24:40 +0000

    new bash

---
+++ scripts/ssl/evaluation/lab/eval_vit-multi_stage-higher_img_resolution-not_use_imi_in_refinement-navhard_two_stage.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Default values
+epoch=${1:-19}
+dir=${2:-"training/ssl/ori/lr_baseline"}
+num_refinement_stage=$3
+stage_layers=$4
+topks=$5
+use_first_stage_traj_in_infer=${6:-"false"}
+
+inference_model="teacher"
+
+# Format epoch with leading zero
+padded_epoch=$(printf "%02d" $epoch)
+
+# Calculate step from epoch (1330 steps per epoch)
+step=$((($epoch + 1) * 1330))
+
+metric_cache_path="${NAVSIM_EXP_ROOT}/metric_cache/navhard_two_stage"
+
+# Set experiment name based on inference model
+if [ "$inference_model" = "teacher" ]; then
+    experiment_name="${dir}/test-${padded_epoch}ep-one_stage-higher_img_resolution-not_use_imi_in_refinement-navhard_two_stage"
+else
+    experiment_name="${dir}/test-${padded_epoch}ep-${inference_model}-one_stage-higher_img_resolution-not_use_imi_in_refinement-navhard_two_stage"
+fi
+
+if [ "$use_first_stage_traj_in_infer" = "true" ]; then
+    experiment_name="$experiment_name-use_first_stage_traj_in_infer"
+fi
+
+command_string="TORCH_NCCL_ENABLE_MONITORING=0 \
+python ${NAVSIM_DEVKIT_ROOT}/navsim/planning/script/run_pdm_score_gpu_ssl.py \
+    +debug=false \
+    +use_pdm_closed=false \
+    agent=hydra_img_vit_ssl \
+    dataloader.params.batch_size=8 \
+    worker.threads_per_node=128 \
+    agent.checkpoint_path='${NAVSIM_EXP_ROOT}/${dir}/epoch\=${padded_epoch}-step\=${step}.ckpt' \
+    agent.config.training=false \
+    agent.config.only_ori_input=true \
+    agent.config.inference.model=${inference_model} \
+    agent.config.camera_width=2048 \
+    agent.config.camera_height=512 \
+    agent.config.img_vert_anchors=16 \
+    agent.config.img_horz_anchors=64 \
+    agent.config.refinement.use_multi_stage=true \
+    agent.config.refinement.num_refinement_stage=$num_refinement_stage \
+    agent.config.refinement.stage_layers=$stage_layers \
+    agent.config.refinement.topks=$topks \
+    agent.config.lab.use_first_stage_traj_in_infer=$use_first_stage_traj_in_infer \
+    agent.config.lab.use_imi_learning_in_refinement=false \
+    experiment_name=${experiment_name} \
+    +cache_path=null \
+    metric_cache_path=${metric_cache_path} \
+    train_test_split=navhard_two_stage \
+    synthetic_sensor_path=$OPENSCENE_DATA_ROOT/navhard_two_stage/sensor_blobs \
+    synthetic_scenes_path=$OPENSCENE_DATA_ROOT/navhard_two_stage/synthetic_scene_pickles
+"
+
+echo "--- COMMAND ---"
+echo $command_string
+echo -e "\n\n"
+
+eval $command_string
+
+
+
+: '
+usage:
+bash scripts/ssl/evaluation/lab/eval_vit-multi_stage-higher_img_resolution-navhard_two_stage.sh \
+  4 training/ssl/teacher_student/rot_30-trans_0-va_0-p_0.5/multi_stage/labs/stage_layers_3-topks_256-higher_img_resolution \
+  1 3 256 true
+'
--- scripts/ssl/evaluation/lab/eval_vit-multi_stage-higher_img_resolution-not_use_imi_in_refinement.sh
+++ scripts/ssl/evaluation/lab/eval_vit-multi_stage-higher_img_resolution-not_use_imi_in_refinement.sh
@@ -6,7 +6,6 @@ dir=${2:-"training/ssl/ori/lr_baseline"}
 num_refinement_stage=$3
 stage_layers=$4
 topks=$5
-use_first_stage_traj_in_infer=${6:-"false"}
 
 inference_model="teacher"
 
@@ -16,21 +15,17 @@ padded_epoch=$(printf "%02d" $epoch)
 # Calculate step from epoch (1330 steps per epoch)
 step=$((($epoch + 1) * 1330))
 
-metric_cache_path="${NAVSIM_EXP_ROOT}/metric_cache/warmup_two_stage"
+metric_cache_path="${NAVSIM_EXP_ROOT}/metric_cache/test/ori"
 
 # Set experiment name based on inference model
 if [ "$inference_model" = "teacher" ]; then
-    experiment_name="${dir}/test-${padded_epoch}ep-one_stage-higher_img_resolution-warmup_two_stage"
+    experiment_name="${dir}/test-${padded_epoch}ep-one_stage-higher_img_resolution-not_use_imi_in_refinement"
 else
-    experiment_name="${dir}/test-${padded_epoch}ep-${inference_model}-one_stage-higher_img_resolution-warmup_two_stage"
-fi
-
-if [ "$use_first_stage_traj_in_infer" = "true" ]; then
-    experiment_name="$experiment_name-use_first_stage_traj_in_infer"
+    experiment_name="${dir}/test-${padded_epoch}ep-${inference_model}-one_stage-higher_img_resolution-not_use_imi_in_refinement"
 fi
 
 command_string="TORCH_NCCL_ENABLE_MONITORING=0 \
-python ${NAVSIM_DEVKIT_ROOT}/navsim/planning/script/run_pdm_score_gpu_ssl.py \
+python ${NAVSIM_DEVKIT_ROOT}/navsim/planning/script/run_pdm_score_one_stage_gpu_ssl.py \
     +debug=false \
     +use_pdm_closed=false \
     agent=hydra_img_vit_ssl \
@@ -48,13 +43,12 @@ python ${NAVSIM_DEVKIT_ROOT}/navsim/planning/script/run_pdm_score_gpu_ssl.py \
     agent.config.refinement.num_refinement_stage=$num_refinement_stage \
     agent.config.refinement.stage_layers=$stage_layers \
     agent.config.refinement.topks=$topks \
-    agent.config.lab.use_first_stage_traj_in_infer=$use_first_stage_traj_in_infer \
+    agent.config.lab.use_first_stage_traj_in_infer=false \
+    agent.config.lab.use_imi_learning_in_refinement=false \
     experiment_name=${experiment_name} \
     +cache_path=null \
     metric_cache_path=${metric_cache_path} \
-    train_test_split=warmup_two_stage \
-    synthetic_sensor_path=$OPENSCENE_DATA_ROOT/warmup_two_stage/sensor_blobs \
-    synthetic_scenes_path=$OPENSCENE_DATA_ROOT/warmup_two_stage/synthetic_scene_pickles
+    train_test_split=navtest \
 "
 
 echo "--- COMMAND ---"
@@ -62,11 +56,3 @@ echo $command_string
 echo -e "\n\n"
 
 eval $command_string
-
-
-: '
-usage:
-bash scripts/ssl/evaluation/lab/eval_vit-multi_stage-higher_img_resolution-warmup_two_stage.sh \
-  5 training/ssl/teacher_student/rot_30-trans_0-va_0-p_0.5/multi_stage/labs/stage_layers_3-topks_256-higher_img_resolution \
-  1 3 256 true
-'
