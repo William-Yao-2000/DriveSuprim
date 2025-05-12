0bc48f43f0ab4eaea07d738722d98e5f8f14b34c
william_yao_2000 <281744213@qq.com>
2025-05-12 12:07:40 +0000

    fix bugs

--- scripts/evaluation/run_ego_mlp_agent_pdm_score_evaluation.sh
+++ scripts/evaluation/run_ego_mlp_agent_pdm_score_evaluation.sh
@@ -1,7 +1,7 @@
 ckpt_epoch=19
 
 TRAIN_TEST_SPLIT=navtest
-CHECKPOINT="${NAVSIM_EXP_ROOT}/ego_mlp_agent/epoch\=${ckpt_epoch}-step\=3340.ckpt"
+CHECKPOINT="${NAVSIM_EXP_ROOT}/training/ego_mlp_agent/epoch\=${ckpt_epoch}-step\=3340.ckpt"
 CACHE_PATH="${NAVSIM_EXP_ROOT}/metric_cache/test/ori"
 
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score.py \
