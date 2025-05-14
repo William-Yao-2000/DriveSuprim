ae7d455114503b160a37fd3e65aecdc55ed4a2bb
william_yao_2000 <281744213@qq.com>
2025-05-14 02:46:47 +0000

    update

--- scripts/evaluation/run_transfuser_pdm_score_evaluation.sh
+++ scripts/evaluation/run_transfuser_pdm_score_evaluation.sh
@@ -7,7 +7,6 @@ CACHE_PATH="${NAVSIM_EXP_ROOT}/metric_cache/test/ori"
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score_one_stage_gpu.py \
     train_test_split=$TRAIN_TEST_SPLIT \
     agent=transfuser_agent \
-    worker=single_machine_thread_pool \
     agent.checkpoint_path=$CHECKPOINT \
     experiment_name=training/transfuser_agent/test-one_stage-${ckpt_epoch}ep \
     metric_cache_path=$CACHE_PATH
