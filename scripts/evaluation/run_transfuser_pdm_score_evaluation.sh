9b6d845ff86b20de48b9eab332808a3c134f2d2e
william_yao_2000 <281744213@qq.com>
2025-05-14 02:35:49 +0000

    update

--- scripts/evaluation/run_transfuser_pdm_score_evaluation.sh
+++ scripts/evaluation/run_transfuser_pdm_score_evaluation.sh
@@ -1,15 +1,13 @@
-TRAIN_TEST_SPLIT=navhard_two_stage
-CHECKPOINT=/path/to/transfuser.ckpt
-CACHE_PATH=YOUR_PATH_TO_METRIC_CACHE
-SYNTHETIC_SENSOR_PATH=$OPENSCENE_DATA_ROOT/navhard_two_stage/sensor_blobs
-SYNTHETIC_SCENES_PATH=$OPENSCENE_DATA_ROOT/navhard_two_stage/synthetic_scene_pickles
+ckpt_epoch=19
 
-python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score.py \
-train_test_split=$TRAIN_TEST_SPLIT \
-agent=transfuser_agent \
-worker=single_machine_thread_pool \
-agent.checkpoint_path=$CHECKPOINT \
-experiment_name=transfuser_agent \
-metric_cache_path=$CACHE_PATH \
-synthetic_sensor_path=$SYNTHETIC_SENSOR_PATH \
-synthetic_scenes_path=$SYNTHETIC_SCENES_PATH \
+TRAIN_TEST_SPLIT=navtest
+CHECKPOINT="${NAVSIM_EXP_ROOT}/training/transfuser_agent/epoch\=${ckpt_epoch}-step\=3340.ckpt"
+CACHE_PATH="${NAVSIM_EXP_ROOT}/metric_cache/test/ori"
+
+python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score_one_stage.py \
+    train_test_split=$TRAIN_TEST_SPLIT \
+    agent=transfuser_agent \
+    worker=single_machine_thread_pool \
+    agent.checkpoint_path=$CHECKPOINT \
+    experiment_name=training/transfuser_agent/test-one_stage-${ckpt_epoch}ep \
+    metric_cache_path=$CACHE_PATH
