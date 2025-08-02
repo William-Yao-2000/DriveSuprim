ebf6b4c8f5bc1a640387aac38fa084403f4b0b0e
william_yao_2000 <281744213@qq.com>
2025-08-02 09:34:00 +0000

    new bash

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -3,6 +3,6 @@
 export NUPLAN_MAP_VERSION="nuplan-maps-v1.0"
 export NUPLAN_MAPS_ROOT="/DATA3/yaowenhao/proj/auto_drive/navsim_workspace/dataset/maps"
 export NAVSIM_EXP_ROOT="/DATA3/yaowenhao/proj/auto_drive/navsim_workspace/exp_v2"
-export NAVSIM_DEVKIT_ROOT="/DATA3/yaowenhao/proj/auto_drive/navsim_workspace/hydramdp_cvpr24"
+export NAVSIM_DEVKIT_ROOT="/DATA3/yaowenhao/proj/auto_drive/navsim_workspace/navsim_ssl_v2"
 export OPENSCENE_DATA_ROOT="/DATA3/yaowenhao/proj/auto_drive/navsim_workspace/dataset"
 export NAVSIM_TRAJPDM_ROOT="/DATA3/yaowenhao/proj/auto_drive/navsim_workspace/dataset/traj_pdm_v2"
