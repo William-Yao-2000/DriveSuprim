d02af49946858066dca9303588cd82ecce421e5c
william_yao_2000 <281744213@qq.com>
2025-10-07 16:58:29 +0000

    fix bug

--- scripts/slurm_bash/ssl/training/teacher_student/multi_stage/labs-r34/imi_diff_thresh.sh
+++ scripts/slurm_bash/ssl/training/teacher_student/multi_stage/labs-r34/imi_diff_thresh.sh
@@ -5,24 +5,26 @@ soft_label_imi_diff_thresh=$3
 
 dir_name=$(echo $bash_file-multi_stage_labs-r34-imi_thresh_$soft_label_imi_diff_thresh | tr '/' '-' | tr '.' 'dot')
 
+PREFIX_PATH=/lustre/fsw/portfolios/av/projects/av_research/users/shiyil/yaowenh
+
 submit_job \
     --gpu 8 \
     --tasks_per_node 8 \
     --nodes 1 \
     -n "$dir_name" \
-    --image /lustre/fsw/portfolios/av/users/shiyil/yaowenh/container_images/ywh-navsim.sqsh \
-    --logroot /lustre/fsw/portfolios/av/users/shiyil/yaowenh/slurm_logs/navsim_v2/training \
+    --image $PREFIX_PATH/container_images/ywh-navsim.sqsh \
+    --logroot $PREFIX_PATH/slurm_logs/navsim_v2/training \
     --email_mode never \
     --duration 4 \
     --dependent_clones 0 \
     --partition $partition \
     --account av_research \
-    -c ". /lustre/fsw/portfolios/av/users/shiyil/yaowenh/pre-navsim_v2.sh; bash $bash_file $soft_label_imi_diff_thresh"
+    -c ". $PREFIX_PATH/pre-navsim_v2.sh; bash $bash_file $soft_label_imi_diff_thresh"
 
 
 : '
 usage:
-bash scripts/slurm_bash/ssl/training/teacher_student/multi_stage/labs-r34/cameras.sh \
+bash scripts/slurm_bash/ssl/training/teacher_student/multi_stage/labs-r34/imi_diff_thresh.sh \
     scripts/ssl/training/teacher_student/rot_30-trans_0-va_0-p_0.5/multi_stage/labs-r34/imi_diff_thresh.sh \
-    interactive_singlenode 0.0
+    interactive 0.0
 '
